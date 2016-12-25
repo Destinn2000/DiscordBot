a48c1c6f25be5761dea271b67839c27009c5e3fa
Destin Nobach <destin.nobach@gmail.com>
2016-12-25 16:28:44 +0000

    Added music command

--- discordbot.js
+++ discordbot.js
@@ -1,18 +1,35 @@
 // Link: http://bit.ly/2hkQM1j
 const Discord = require('discord.js');
+const YouTube = require('youtube-node');
 const Auth = require('./auth.json');
 
 const bot = new Discord.Client();
+const youTube = new YouTube();
 
 const token = Auth.d_token;
+youTube.setKey(Auth.y_token);
 
 bot.on('ready', () => {
 	console.log('Bot joined ' + bot.guilds.array());
 });
 
 bot.on('message', message => {
-	if(message.content == '!ping') {
-		message.channel.sendMessage('Pong');
+	const rawmsg = message.content;
+	if(rawmsg.startsWith('!') && !(rawmsg.indexOf(' ') >= 0)) {
+		const command = rawmsg.toLowerCase();
+
+		switch(command) {
+			case '!ping':
+				message.channel.sendMessage('Pong');
+			break;
+		}
+	}
+	else if(rawmsg.startsWith('!music')) {
+		const result = rawmsg.split(" ");
+
+		if(result.length == 2) {
+			message.channel.sendMessage(JSON.stringify(result));
+		}
 	}
 });
 
